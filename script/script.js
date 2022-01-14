cf31c6da54f0db6cda7db2919cd49f738465a914
lucianoaraus <lucianoaraus@gmail.com>
2022-01-13 21:41:56 -0300

    add new task done, remove function -> wip

--- script/script.js
+++ script/script.js
@@ -8,7 +8,15 @@ class Task {
   }
 }
 
-const currentsTask = [];
+const currentTasks = [];
+
+function deleteTask(id) {
+  console.log("entro");
+  const toDelete = document.getElementById("taskDiv" + id);
+  console.log(toDelete);
+  console.log(document);
+  document.removeChild(toDelete); /* ARREGLAR */
+}
 
 function addTask() {
   const newTaskText = document.getElementById("new-task-text");
@@ -16,30 +24,30 @@ function addTask() {
     const table = document.getElementById("table");
     const newContent = document.createElement("div");
 
-    currentsTask.push(new Task(currentsTask.length, newTaskText.value));
-    console.log(currentsTask);
+    currentTasks.push(new Task(currentTasks.length, newTaskText.value));
+    console.log(currentTasks);
     newContent.innerHTML = `
-      <li class="list-group-item">
+      <li class="list-group-item" id="taskDiv${currentTasks.length}">
         <div class="widget-content p-0">
           <div class="widget-content-wrapper">
             <div class="widget-content-left mr-2">
               <div class="custom-checkbox custom-control">
-                <input
-                  class="custom-control-input"
-                  id="exampleCustomCheckbox12"
-                  type="checkbox"
-                /><label
-                  class="custom-control-label"
-                  for="exampleCustomCheckbox12"
-                ></label>
-              </div>
+              <input
+                class="custom-control-input"
+                id="exampleCustomCheckbox${currentTasks.length}" 
+                type="checkbox"
+              /><label
+                class="custom-control-label"
+                for="exampleCustomCheckbox${currentTasks.length}"
+              ></label>
+            </div>
             </div>
             <div class="widget-content-left">
               <div class="widget-heading">
                 ${newTaskText.value}
               </div>
               <div class="widget-subheading">
-                <i>By CAMBIAR POR NOMBRE DE USUARIO</i> 
+                <!-- <i>By CAMBIAR POR NOMBRE DE USUARIO</i> -->
               </div>
             </div>
             <div class="widget-content-right">
@@ -49,7 +57,8 @@ function addTask() {
               <button
                 class="border-0 btn-transition btn btn-outline-danger"
               >
-                <i class="fa fa-trash"></i>
+                <i class="fa fa-trash" id="trash${currentTasks.length}" onClick="deleteTask(${currentTasks.length})"></i>
+
               </button>
             </div>
           </div>
@@ -60,4 +69,7 @@ function addTask() {
   } else {
     console.log("empty task");
   }
+  console.log("lenght al momento de crear nueva tarea" + currentTasks.length);
+  console.log("table despues de insertar child");
+  console.log(table);
 }
