3691705e1cd008a607a3f8728b87fe5b96205542
lucianoaraus <lucianoaraus@gmail.com>
2022-01-15 23:13:24 -0300

    uploading changes

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# challenge-ensolvers-lucho
+# Challenge Ensolvers
+
+This proyect was made for the Ensolvers challenge. Owner: Luciano Araus
+
+## Decisions:
+
+The project started with the idea of being done with React, but then it ended up being done with pure vanilla javascript with the aim of later migrating to React.
+
+## Data structure:
+
+For the format of organizing tasks by folders, I decided to use a Class structure with arrays.
+
+## Frontend (WIP):
+
+- HTML, CSS, Vanilla Javascript.
+- Libraries: Bootsrap, JQuery.
+
+## Backend (TODO):
+
+- Node.js and Sequalize (ORM)
+- MariaDB (DB)
+- Software: HeidiSQL
+
+## Extras (TODO):
+
+Deploy: netlify
+Login:
+
+- Option 1: DB
+- Option 2: Google API
--- script/script.js
+++ script/script.js
@@ -1,50 +1,168 @@
-//<input type="text" id="new-task-input" placeholder="New Task" />
-//<button class="btn btn-primary">Add Task</button>
+class FoldersList {
+  constructor(id, folders) {
+    this.id = id;
+    this.folders = folders; /* array de carpetas */
+    this.div = document.createElement("div").innerHTML = `
+      <div class="scroll-area-sm">
+        <perfect-scrollbar class="ps-show-limits">
+          <div style="position: static" class="ps ps--active-y">
+            <div class="ps-content">
+              <ul class="list-group list-group-flush" id="table">
+              </ul>
+            </div>
+          </div>
+        </perfect-scrollbar>
+      </div>
+    `;
+  }
+
+  addFolderListHTML() {
+    document
+      .getElementById("folders-header")
+      .insertAdjacentHTML("afterend", this.div);
+  }
+
+  addFolder(newFolder) {
+    this.folders.push(newFolder);
+    newFolder.addFolderHTML();
+  }
+
+  deleteFolder(deleteFolder) {
+    /*WIP*/
+    let newArray = [];
+    newArray = this.folders.filter(
+      (folder) => folder.id != deleteFolder.id
+    ); /* Filtra por id y saca la carpeta no deseada */
+    this.folders = newArray; /* Se actualiza con las nuevas carpetas */
+  }
+}
+
+let foulderCount = 0; /* For the Folders IDs */
+$("#new-folder-button").click(function () {
+  const newFolder = document.getElementById("new-folder-text");
+  const newFolderValue = newFolder.value;
+
+  if (newFolderValue.length > 0) {
+    const newFolderObject = new Folder(foulderCount, newFolderValue, []);
+    motherFolderList.addFolder(newFolderObject);
+    console.log("new folder created");
+    foulderCount++;
+  } else {
+    console.log("no valid folder name");
+  }
+});
+
+$("#test").click(function () {
+  console.log("entro");
+});
+
+const motherFolderList = new FoldersList(0, []); /* MUST BE UNIQUE, ONLY ONE */
+
+/* ------------------------     Creacion del codigo HTML -------------------- */
+motherFolderList.addFolderListHTML();
+
+let taskCount = 0; /* For the Tasks IDs */
+class Folder {
+  constructor(id, name, tasks) {
+    this.id = id;
+    this.name = name;
+    this.tasks = tasks; /* array of tasks */
+    this.div = document.createElement("div").innerHTML = `
+      <li class="list-group-item" id="folder${id}DivId">
+        <div class="widget-content p-0">
+          <div class="widget-content-wrapper">
+            <div class="widget-content-left mr-2">
+            <div class="custom-checkbox custom-control">
+            <input
+              class="custom-control-input"
+              id="exampleCustomCheckbox${id}" 
+              type="checkbox"
+            /><label
+              class="custom-control-label"
+              for="exampleCustomCheckbox${id}"
+            ></label>
+          </div>
+          </div>
+            <div class="widget-content-left">
+              <div class="widget-heading">
+                ${name}
+              </div>
+            </div>
+            <div class="widget-content-right">
+              <button class="border-0 btn btn-outline-success" id="folder${id}ItemsButtonId">
+                view items
+              </button>
+              <button
+                class="border-0 btn-transition btn btn-outline-danger" id="test"
+              >
+                <i class="fa fa-trash"></i>
+              </button>
+            </div>
+          </div>
+        </div>
+      </li>
+    `;
+  }
+
+  addFolderHTML() {
+    /*Realizar instantaneamente al crear una nueva carpeta */
+    document.getElementById("table").insertAdjacentHTML("beforeend", this.div);
+  }
+
+  addTask(newTaskContent) {
+    const newTask = new Task(taskCount, newTaskContent);
+    this.tasks.push(newTask);
+  }
+
+  deleteTask(deleteTask) {
+    console.log("entro delete", deleteTask);
+    let newArray = [];
+    newArray = this.tasks.filter(
+      (tasks) => tasks.id != deleteTask.id
+    ); /* Filtra por id y saca la tarea no deseada */
+    this.tasks = newArray; /* Se actualiza con las nuevas tareas */
+  }
+
+  switchFooterToCreateTaskUI() {
+    console.log("entro");
+    const newFooter = document.createElement("div");
+    newFooter.innerHTML = `
+      <div class="card-footer" id="task-footer-create-ui">
+        <input type="text" id="new-folder-text" placeholder="New Task" class="footer-input">
+        <button type="button" class="btn btn-primary" id="new-folder-button">
+          Add Task
+        </button>
+      </div>
+    `;
+
+    document.getElementById("foolder-footer").appendChild(newFooter);
+    //pintar la tarea seleccionada de otro color
+  }
+}
 
 class Task {
   constructor(id, content) {
     this.id = id;
     this.content = content;
-  }
-}
-
-const currentTasks = [];
-
-function deleteTask(id) {
-  console.log("entro");
-  const toDelete = document.getElementById("taskDiv" + id);
-  console.log(toDelete);
-  console.log(document);
-  document.removeChild(toDelete); /* ARREGLAR */
-}
-
-function addTask() {
-  const newTaskText = document.getElementById("new-task-text");
-  if (newTaskText.value.length > 0) {
-    const table = document.getElementById("table");
-    const newContent = document.createElement("div");
-
-    currentTasks.push(new Task(currentTasks.length, newTaskText.value));
-    console.log(currentTasks);
-    newContent.innerHTML = `
-      <li class="list-group-item" id="taskDiv${currentTasks.length}">
+    this.div = document.createElement("div").innerHTML = `
+      <li class="list-group-item" id="taskDiv${id}">
         <div class="widget-content p-0">
           <div class="widget-content-wrapper">
             <div class="widget-content-left mr-2">
               <div class="custom-checkbox custom-control">
               <input
                 class="custom-control-input"
-                id="exampleCustomCheckbox${currentTasks.length}" 
+                id="exampleCustomCheckbox${id}" 
                 type="checkbox"
               /><label
                 class="custom-control-label"
-                for="exampleCustomCheckbox${currentTasks.length}"
+                for="exampleCustomCheckbox${id}"
               ></label>
             </div>
             </div>
             <div class="widget-content-left">
-              <div class="widget-heading">
-                ${newTaskText.value}
+              <div class="widget-heading" id="taskText${id}">
+                ${content}
               </div>
               <div class="widget-subheading">
                 <!-- <i>By CAMBIAR POR NOMBRE DE USUARIO</i> -->
@@ -52,24 +170,20 @@ function addTask() {
             </div>
             <div class="widget-content-right">
               <button class="border-0 btn btn-outline-success">
-                <a href="#">Edit</a>
+                <a id="editA${id}" onClick="switchFooter(${id})">Edit</a>
               </button>
               <button
                 class="border-0 btn-transition btn btn-outline-danger"
               >
-                <i class="fa fa-trash" id="trash${currentTasks.length}" onClick="deleteTask(${currentTasks.length})"></i>
-
+                <i class="fa fa-trash" id="trash${id}" onClick="deleteTask(${id})"></i>
               </button>
             </div>
           </div>
         </div>
       </li>
-      `;
-    table.appendChild(newContent);
-  } else {
-    console.log("empty task");
+    `;
   }
-  console.log("lenght al momento de crear nueva tarea" + currentTasks.length);
-  console.log("table despues de insertar child");
-  console.log(table);
+  editContent(newContent) {
+    this.content = newContent;
+  }
 }
